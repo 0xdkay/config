bc4f3cff959bdaca61debe1662559cd6b51bccc4
Dongkwan Kim <insinoa@gmail.com>
2014-11-13 22:26:42 +0900

    add other applications

---
+++ fourth.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+sudo apt-get install -y gdb
+sudo apt-get install -y python-pip
+
+gem install json
+gem install net-ssh
+gem install nokogiri
+gem install sqlite3
+gem install rake
+gem install mechanize
+
